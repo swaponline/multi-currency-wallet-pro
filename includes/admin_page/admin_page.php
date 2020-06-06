0517dab40ee60a51452bf12804a13ba1f8021f64
BootWP <burdianov@gmail.com>
2020-06-06 11:51:25 +0300

    Change upaded date format

--- includes/admin_page/admin_page.php
+++ includes/admin_page/admin_page.php
@@ -440,9 +440,8 @@ function mcwallet_page() {
 	<div class="mcwallet-info-bar">
 		<?php
 			$filename = MCWALLET_PATH . 'multi-currency-wallet-pro.php';
-			$update_time = date("F d, Y - H:i", filemtime( $filename ) );
-
-			printf( esc_html__( 'Plugin version: %s | Build version: %s | Updated: %s', 'multi-currency-wallet' ), MCWALLET_VER, MCWALLET_BUILD_VER, $update_time );
+			$update_time = gmdate( 'H\h. i\m. s\s.', time() - filectime( $filename ) );
+			printf( esc_html__( 'Plugin version: %s | Build version: %s | Updated: %s ago', 'multi-currency-wallet' ), MCWALLET_VER, MCWALLET_BUILD_VER, $update_time );
 		?>
 	</div>
 </div><!-- .welcome-panel -->
