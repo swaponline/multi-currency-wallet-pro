6982de6b6bfaab9ebadc530f25d3b4fcb684f8d3
BootWP <burdianov@gmail.com>
2020-06-06 11:59:45 +0300

    Change time format

--- includes/tags.php
+++ includes/tags.php
@@ -11,8 +11,8 @@ function mcwallet_info_bar_markup() {
 	<div class="mcwallet-info-bar">
 		<?php
 			$filename = MCWALLET_PATH . 'multi-currency-wallet-pro.php';
-			$update_time = gmdate( 'H\h. i\m. s\s.', time() - filectime( $filename ) );
-			printf( esc_html__( 'Plugin version: %s | Build version: %s | Updated: %s ago', 'multi-currency-wallet' ), MCWALLET_VER, MCWALLET_BUILD_VER, $update_time );
+			$update_time = gmdate( 'H\h : i\m : s\s', time() - filectime( $filename ) );
+			printf( esc_html__( 'Plugin version: %s | Build version: %s | Updated: %s ago.', 'multi-currency-wallet' ), MCWALLET_VER, MCWALLET_BUILD_VER, $update_time );
 		?>
 	</div>
 	<?php
