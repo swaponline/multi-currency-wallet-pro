bc27f67d2fa65f9121f934b33b07e777013c7b2d
Noxon <2914674+noxonsu@users.noreply.github.com>
2020-07-10 13:07:59 +0300

    Update load-app.php

--- includes/enqueue_scripts/load-app.php
+++ includes/enqueue_scripts/load-app.php
@@ -9,27 +9,42 @@ if ( version_compare( PHP_VERSION, '7.0.0' ) >= 0) {
 	$path_levels = dirname( __FILE__ ) . '../../../../../../';
 }
 
+$version = md5((isset($_GET['ver'])) ? $_GET['ver'] : 'no');
+$cache_file = 'wp-content/uploads/swap-wallet-app-' . $version . '.js';
+
 require_once $path_levels . 'wp-load.php';
 
-$path = MCWALLET_PATH . 'vendors/swap/app.js';
+if (file_exists($path_levels . $cache_file)) {
+  header("Cache-control: public");
+  header("Expires: " . gmdate("D, d M Y H:i:s", time() + 60*60*24) . " GMT");
+  header( 'Content-Type: application/javascript; charset=UTF-8' );
+  echo file_get_contents($path_levels . $cache_file);
 
-$app  = file_get_contents( $path );
+} else {
+  $path = MCWALLET_PATH . 'vendors/swap/app.js';
 
-$strings = get_option( 'mcwallet_strings' );
+  $app  = file_get_contents( $path );
 
-if ( $strings ) {
-	foreach ( $strings as $string ) {
-		$key                  = '"' . $string[0] . '"';
-		$value                = '"' . $string[1] . '"';
-		$replacements[ $key ] = $value;
-	}
-	if ( $replacements ) {
-		$app = str_replace( array_keys( $replacements ), $replacements, $app );
-	}
+  $strings = get_option( 'mcwallet_strings' );
+
+  if ( $strings ) {
+    foreach ( $strings as $string ) {
+      $key                  = '"' . $string[0] . '"';
+      $value                = '"' . $string[1] . '"';
+      $replacements[ $key ] = $value;
+    }
+    if ( $replacements ) {
+      $app = str_replace( array_keys( $replacements ), $replacements, $app );
+    }
+  }
+
+
+  file_put_contents($path_levels . $cache_file, $app);
+
+  header("Cache-control: public");
+  header("Expires: " . gmdate("D, d M Y H:i:s", time() + 60*60*24) . " GMT");
+  header( 'Content-Type: application/javascript; charset=UTF-8' );
+
+  echo $app;
 }
-
-header( 'Content-Type: application/javascript; charset=UTF-8' );
-
-echo $app;
-
 exit;
