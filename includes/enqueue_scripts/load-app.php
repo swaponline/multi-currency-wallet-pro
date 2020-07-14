ce6250bfcf6b7f6da7ace808dc9d06599fb2937d
Noxon <2914674+noxonsu@users.noreply.github.com>
2020-07-14 21:07:49 +0300

    Update load-app.php

--- includes/enqueue_scripts/load-app.php
+++ includes/enqueue_scripts/load-app.php
@@ -9,44 +9,47 @@ if ( version_compare( PHP_VERSION, '7.0.0' ) >= 0) {
 	$path_levels = dirname( __FILE__ ) . '../../../../../../';
 }
 
-$version    = md5( ( isset( $_GET['ver'] ) ) ? $_GET['ver'] : 'no' );
-$cache_file = 'wp-content/uploads/swap-wallet-app-' . $version . '.js';
+$_GET_ver = (isset($_GET['ver'])) ? $_GET['ver'] : false;
+
+
+
 
 require_once $path_levels . 'wp-load.php';
 
-if ( file_exists($path_levels . $cache_file) ) {
-	header( "Cache-control: public" );
-	header( "Expires: " . gmdate( "D, d M Y H:i:s", time() + 60*60*24 ) . " GMT" );
-	header( 'Content-Type: application/javascript; charset=UTF-8' );
-	echo file_get_contents( $path_levels . $cache_file );
+$version = md5((($_GET_ver) ? $_GET_ver : ((MCWALLET_VER) ? MCWALLET_VER : 'no'));
+$cache_file = 'wp-content/uploads/swap-wallet-app-' . $version . '.js';
+
+if (file_exists($path_levels . $cache_file)) {
+  header("Cache-control: public");
+  header("Expires: " . gmdate("D, d M Y H:i:s", time() + 60*60*24) . " GMT");
+  header( 'Content-Type: application/javascript; charset=UTF-8' );
+  echo file_get_contents($path_levels . $cache_file);
 
 } else {
-	$path = MCWALLET_PATH . 'vendors/swap/app.js';
+  $path = MCWALLET_PATH . 'vendors/swap/app.js';
 
-	$app  = file_get_contents( $path );
+  $app  = file_get_contents( $path );
 
-	$strings = get_option( 'mcwallet_strings' );
+  $strings = get_option( 'mcwallet_strings' );
 
-	if ( $strings ) {
-		$replacements = array();
-		foreach ( $strings as $string ) {
-			if ( isset( $string[0] ) && isset( $string[1] ) ) {
-				$key                  = '"' . $string[0] . '"';
-				$value                = '"' . $string[1] . '"';
-				$replacements[ $key ] = $value;
-			}
-		}
-		if ( $replacements ) {
-			$app = str_replace( array_keys( $replacements ), $replacements, $app );
-		}
-	}
+  if ( $strings ) {
+    foreach ( $strings as $string ) {
+      $key                  = '"' . $string[0] . '"';
+      $value                = '"' . $string[1] . '"';
+      $replacements[ $key ] = $value;
+    }
+    if ( $replacements ) {
+      $app = str_replace( array_keys( $replacements ), $replacements, $app );
+    }
+  }
 
-	file_put_contents($path_levels . $cache_file, $app);
 
-	header("Cache-control: public");
-	header("Expires: " . gmdate("D, d M Y H:i:s", time() + 60*60*24) . " GMT");
-	header( 'Content-Type: application/javascript; charset=UTF-8' );
+  file_put_contents($path_levels . $cache_file, $app);
 
-	echo $app;
+  header("Cache-control: public");
+  header("Expires: " . gmdate("D, d M Y H:i:s", time() + 60*60*24) . " GMT");
+  header( 'Content-Type: application/javascript; charset=UTF-8' );
+
+  echo $app;
 }
 exit;
