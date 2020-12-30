33669bef2c24c0cf63589fbb3b74576acbf2f0a4
developer24-exe <68427882+developer24-exe@users.noreply.github.com>
2020-12-30 21:00:52 +0000

    Revert "splash not in root (need for show splash when restore user)"

--- template/template.php
+++ template/template.php
@@ -22,34 +22,35 @@ do_action( 'mcwallet_before_template' );
 <body>
 <?php mcwallet_body_open(); ?>
 
-	<div id="root"></div><!-- #root -->
-  <!-- Loader before any JS -->
-  <div id="wrapper_element" class="overlay">
-    <div class="center">
-      <div id="loader" class="loader">
-        <img id="loaderImg" class="logo-light" src="<?php echo esc_url( mcwallet_logo_url() ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>">
-        <img id="loaderImg" class="logo-dark" src="<?php echo esc_url( mcwallet_dark_logo_url() ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>">
+	<div id="root">
+		<!-- Loader before any JS -->
+		<div id="wrapper_element" class="overlay">
+			<div class="center">
+				<div id="loader" class="loader">
+					<img id="loaderImg" class="logo-light" src="<?php echo esc_url( mcwallet_logo_url() ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>">
+					<img id="loaderImg" class="logo-dark" src="<?php echo esc_url( mcwallet_dark_logo_url() ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>">
+				</div>
+				<div id="beforeJSTip" class="tips">
+					<?php esc_html_e( 'Do not forget to save your private keys!', 'multi-currency-wallet' );?>
+				</div>
+			</div>
+      <div class="mb-4 show-on-fail-ls d-none" id="onFailLocalStorageMessage">
+        <span>Not every function works In this window, please open new tab.
+          If the error will repeat please contact admin
+          <br />
+          <a href="https://t.me/sashanoxon">
+            https://t.me/sashanoxon
+          </a>
+        </span>
+        <button class="btn btn-primary btc-open-in-new-tab">
+          <a href="https://wallet.wpmix.net" id="onFailLocalStorageLink" target="_blank">
+            Open App in new tab
+          </a>
+        </button>
       </div>
-      <div id="beforeJSTip" class="tips">
-        <?php esc_html_e( 'Do not forget to save your private keys!', 'multi-currency-wallet' );?>
-      </div>
-    </div>
-    <div class="mb-4 show-on-fail-ls d-none" id="onFailLocalStorageMessage">
-      <span>Not every function works In this window, please open new tab.
-        If the error will repeat please contact admin
-        <br />
-        <a href="https://t.me/sashanoxon">
-          https://t.me/sashanoxon
-        </a>
-      </span>
-      <button class="btn btn-primary btc-open-in-new-tab">
-        <a href="https://wallet.wpmix.net" id="onFailLocalStorageLink" target="_blank">
-          Open App in new tab
-        </a>
-      </button>
-    </div>
-    <div id="usersInform" class="usersInform"></div>
-  </div>
+			<div id="usersInform" class="usersInform"></div>
+		</div>
+	</div><!-- #root -->
 
 	<div id="portal"></div>
 
