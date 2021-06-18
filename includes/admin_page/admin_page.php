3271e234b2f285186f76f0671bd380d8f0a4baeb
Noxon <2914674+noxonsu@users.noreply.github.com>
2021-06-18 17:11:16 +0300

    Update admin_page.php

--- includes/admin_page/admin_page.php
+++ includes/admin_page/admin_page.php
@@ -189,7 +189,7 @@ function mcwallet_page() {
 							</tr>
 							<tr>
 								<th scope="row">
-									<?php esc_html_e( 'Disable sending statistics', 'multi-currency-wallet' );?>
+									<?php esc_html_e( 'Disable sending statistics to plugin developers', 'multi-currency-wallet' );?>
 								</th>
 								<td>
 									<label for="mcwallet_disable_stats">
