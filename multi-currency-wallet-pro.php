a142a0774f9f7fbc7f8e2151dcd9dcd37e67ceff
BootWP <burdianov@gmail.com>
2021-06-27 20:21:42 +0300

    Add admin notice for adding or renew license.

--- multi-currency-wallet-pro.php
+++ multi-currency-wallet-pro.php
@@ -3,7 +3,7 @@
  * Plugin Name: Multi Currency Wallet Pro
  * Plugin URI: https://swaponline.io
  * Description: Simplest Multi-currency wallet for WordPress.
- * Version: 1.1.1190
+ * Version: 1.1.1191
  * Requires at least: 5.4.950
  * Requires PHP: 5.954
  * Author: NoxonThemes
@@ -20,7 +20,7 @@ defined( 'ABSPATH' ) || die( 'Soarele luceste!' );
 /* Define Plugin Constants */
 define( 'MCWALLET_PATH', plugin_dir_path( __FILE__ ) );
 define( 'MCWALLET_URL', plugin_dir_url( __FILE__ ) );
-define( 'MCWALLET_VER', '1.1.1190' );
+define( 'MCWALLET_VER', '1.1.1191' );
 define( 'MCWALLET_BUILD_VER', 'df8396' );
 
 /**
