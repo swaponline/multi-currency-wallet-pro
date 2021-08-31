d4d1aba7488a7a028e16ae693bbdfde84aa27e80
developer24-exe <68427882+developer24-exe@users.noreply.github.com>
2021-08-31 10:42:03 +0000

    Update multi-currency-wallet-pro.php

--- multi-currency-wallet-pro.php
+++ multi-currency-wallet-pro.php
@@ -4,8 +4,8 @@
  * Plugin URI: https://swaponline.io
  * Description: Simplest Multi-currency wallet for WordPress.
  * Version: 1.1.1264
- * Requires at least: 5.4
- * Requires PHP: 5
+ * Requires at least: 5.3.1001
+ * Requires PHP: 5.0.1001
  * Author: NoxonThemes
  * Author URI: https://themeforest.net/user/noxonthemes
  * Text Domain: multi-currency-wallet
