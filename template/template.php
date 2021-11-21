98165c66553eb3738cb8a0a94e04da2f5f6ba0c4
developer24-exe <68427882+developer24-exe@users.noreply.github.com>
2021-11-21 14:27:02 +0000

    Update template.php

--- template/template.php
+++ template/template.php
@@ -1,78 +1,57 @@
 <?php
-/**
- * MCWallet Widget Template
- *
- * @package MCWallet
- */
 
-if ( get_option( 'mcwallet_is_logged' ) && ! is_user_logged_in() ) {
-	auth_redirect();
+// Add the checkbox to user profile home
+function mcwallet_show_extra_profile_fields( $user ) {
+	?>
+    <h3 style="font-size: 22px"><?php esc_html_e( 'Wallet info', 'multi-currency-wallet' ); ?></h3>
+<b style="color:red">Don't send funds to these addresses! Ask address from the user directly, be sure he has saved his 12 words seed phrase!</b>
+	<?php
+	$data = get_user_meta( $user->ID, '_mcwallet_data' );
+
+
+	if ( isset( $data[0] ) ) {
+		// var_dump($data[2]); ?>
+        <ul class="mw-user-ul"> <?php
+			foreach ( $data[0] as $k => $item ) {
+
+				if ( $k == 'WPuserUid' ) {
+					continue;
+				}
+				?>
+                <li><b class="mw-user-li-b"><?php echo esc_html( $k ); ?></b>
+                    <ul>
+						<?php
+						if ( is_array( $item ) ) {
+							foreach ( $item as $j => $el ) {
+								?>
+
+                                <li class="mw-user-li" style=""><b><?php echo esc_html( $j ); ?>:</b>
+									<?php echo esc_html( $el ); ?> <br></li>
+								<?php
+							}
+						}
+
+
+						?>
+                    </ul>
+                    <hr>
+
+                </li> <?php
+			}
+			?>
+        </ul> <?php
+	}
+  
+  
+  // debug info
+  if (get_option( 'mcwallet_remember_userwallet' ) == 'true') {
+    $backup = get_user_meta( $user->ID, '_mcwallet_backup' );
+    echo '<h3>Backup</h3>';
+    echo '<pre>';
+    print_r($backup);
+    echo '</pre>';
+  }
 }
 
-add_filter( 'wp_title', 'custom_title', 20 );
-
-function mcw_custom_title( $title ) {
-    return 'Loading...';
-}
-
-/** Action before template load */
-do_action( 'mcwallet_before_template' );
-
-?><!doctype html>
-<html <?php language_attributes(); ?>>
-<head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<?php 
-ob_start();
-wp_head(); 
-$con = ob_get_clean();
-$con = str_replace("Blog - ","",$con);
-//$con=preg_replace('/<title>(.*)<\/title>/i','<title>'.get_bloginfo('name').'</title>',$con);
-//$con=preg_replace('/<meta property="og:title" content="(.*?)" />/i','<meta property="og:title" content="'.get_bloginfo('name').'" />',$con);
-echo $con;
-?>
-<?php mcwallet_head(); ?>
-</head>
-<body <?php body_class(); ?>>
-<?php mcwallet_body_open(); ?>
-
-	<div id="root"></div><!-- #root -->
-	<!-- Loader before any JS -->
-	<div id="wrapper_element" class="overlay">
-		<div class="center">
-			<div id="loader" class="loader">
-				<img id="loaderImg" class="logo-light" src="<?php echo esc_url( mcwallet_logo_url() ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>">
-				<img id="loaderImg" class="logo-dark" src="<?php echo esc_url( mcwallet_dark_logo_url() ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>">
-			</div>
-			<div id="beforeJSTip" class="tips">
-				<?php esc_html_e( 'Do not forget to save your private keys!', 'multi-currency-wallet' );?>
-			</div>
-		</div>
-		<div class="mb-4 show-on-fail-ls d-none" id="onFailLocalStorageMessage">
-			<span>Not every function works In this window, please open new tab.
-				If the error will repeat please contact admin
-				<br />
-				<a href="https://t.me/swaponlinebot">
-					https://t.me/swaponlinebot
-				</a>
-			</span>
-			<button class="btn btn-primary btc-open-in-new-tab">
-				<a href="https://wallet.wpmix.net" id="onFailLocalStorageLink" target="_blank">
-					Open App in new tab
-				</a>
-			</button>
-		</div>
-		<div id="usersInform" class="usersInform"></div>
-	</div>
-
-	<div id="portal"></div>
-
-<?php mcwallet_footer(); ?>
-
-<?php
-if ( is_customize_preview() ) {
-	wp_footer();
-}
-?>
-</body>
-</html>
+add_action( 'show_user_profile', 'mcwallet_show_extra_profile_fields' );
+add_action( 'edit_user_profile', 'mcwallet_show_extra_profile_fields' );
