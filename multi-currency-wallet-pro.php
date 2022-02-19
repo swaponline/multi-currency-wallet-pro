beca361874f586f8b4f2a46bd7c68fd40bcd2569
BootWP <burdianov@gmail.com>
2022-02-19 12:46:20 +0200

    Check if defined 'MCWALLET'

--- multi-currency-wallet-pro.php
+++ multi-currency-wallet-pro.php
@@ -18,14 +18,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
+if ( ! defined( 'MCWALLET' ) ) {
 
-define( 'MCWALLET', true );
-define( 'MCWALLET_PATH', plugin_dir_path( __FILE__ ) );
-define( 'MCWALLET_URL', plugin_dir_url( __FILE__ ) );
-define( 'MCWALLET_FILE', __FILE__ );
-define( 'MCWALLET_VER', '1.1.1421' );
-
-define( 'MCWALLET_BUILD_VER', 'd43633' );
+	/* Define Constants */
+	define( 'MCWALLET', true );
+	define( 'MCWALLET_PATH', plugin_dir_path( __FILE__ ) );
+	define( 'MCWALLET_URL', plugin_dir_url( __FILE__ ) );
+	define( 'MCWALLET_FILE', __FILE__ );
+	define( 'MCWALLET_VER', '1.1.1421' );
+	define( 'MCWALLET_BUILD_VER', 'd43633' );
 
 	/**
 	 * Plugin Init
