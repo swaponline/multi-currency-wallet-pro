8e752374f2986b63384e6ccff1e56842caa44d10
BootWP <burdianov@gmail.com>
2022-02-19 12:14:47 +0200

    Fix conflict between free and pro versions
    Fix conflict between free and pro versions if both are activated.

--- multi-currency-wallet-pro.php
+++ multi-currency-wallet-pro.php
@@ -18,15 +18,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
-/* Define Constants */
-define( 'MCWALLET', true );
-define( 'MCWALLET_PATH', plugin_dir_path( __FILE__ ) );
-define( 'MCWALLET_URL', plugin_dir_url( __FILE__ ) );
-define( 'MCWALLET_FILE', __FILE__ );
-define( 'MCWALLET_VER', '1.1.1420' );
-define( 'MCWALLET_BUILD_VER', 'fc7d57' );
+if ( ! defined( 'MCWALLET' ) ) {
 
-/**
- * Plugin Init
- */
-require MCWALLET_PATH . 'includes/init.php';
+	/* Define Constants */
+	define( 'MCWALLET', true );
+	define( 'MCWALLET_PATH', plugin_dir_path( __FILE__ ) );
+	define( 'MCWALLET_URL', plugin_dir_url( __FILE__ ) );
+	define( 'MCWALLET_FILE', __FILE__ );
+	define( 'MCWALLET_VER', '1.1.1420' );
+	define( 'MCWALLET_BUILD_VER', 'fc7d57' );
+
+	/**
+	 * Plugin Init
+	 */
+	require MCWALLET_PATH . 'includes/init.php';
+
+}
